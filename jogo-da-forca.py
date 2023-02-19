932d5fd51ba0009844b89de9c56100c4371207fd
André Júnior <andrejunior.pessoal@gmail.com>
2023-02-19 17:02:54 -0400

    readme e validação de entrada adicionados

--- jogo-da-forca.py
+++ jogo-da-forca.py
@@ -1,30 +1,47 @@
 from random import randint
 
 
-def validacaoEntrada(tentativa, proibidas, letras_digitadas):
-    while tentativa in letras_digitadas:
-        print('Você já digitou essa palavra!')
-        tentativa = str(input('Digite uma letra: '))
-    while (tentativa in proibidas) or (tentativa.isnumeric()):
+def validarAcentuacao(letra_digitada):
+    letras_proibidas = ['ã', 'á', 'à', 'â', 'í', 'ì', 'î',
+                        'é', 'è', 'ê', 'ç', 'ó', 'ô', 'õ', 'ò']
+    for letra in letra_digitada:
+        if letra in letras_proibidas:
+            return False
+    return True
+
+
+def validacaoEntrada(letra_digitada, letras_digitadas):
+    validacao = letra_digitada.isalpha()
+    if validacao == False:
         print('Desculpa, mas não aceitamos esse tipo caracteres')
-        tentativa = str(input('Digite uma letra: '))
-    return tentativa
+        return False
+    validacao = validarAcentuacao(letra_digitada)
+    if validacao == False:
+        print('Desculpa, mas não aceitamos esse tipo caracteres')
+        return False
+    if letra_digitada in letras_digitadas:
+        if len(letra_digitada) > 1:
+            print('Você já digitou essa palavra!')
+        else:
+            print('Você já digitou essa letra!')
+        return False
+    else:
+        return True
 
 
 file = open('palavras.txt')
 palavras = file.readlines()
-dados = palavras[randint(0, len(palavras))]
+dados = palavras[randint(1, len(palavras))]
 
-letras_proibidas = ['ã', 'á', 'à', 'â', 'ã', 'í', 'ì', 'î', 'é', 'è', 'ê', 'ç']
 dados = dados.split(';')
 palavra = dados[0]
 dica = dados[1]
-palavra_misteriosa = []
-letras_da_palavra = []
-letras_digitadas = []
-tamanho_tabulacao = len(dica)
+palavra_misteriosa = []  # Os tracinho que vai mostrar para o usuario
+letras_da_palavra = []  # Todas as letras da palavra, acertou a letra, ela é removida
+letras_digitadas = []  # Input do usuário
+tamanho_tabulacao = len(dica)  # Tabulação das linhas e etc.
 vida = 6
-acertos = ''
+acertos = ''  # Palavras acertadas
 
 print('Por favor, não digite palavras com acento, ou caraceter especial.')
 
@@ -56,32 +73,36 @@ while (acertos != palavra) and (vida > 0):
         print('-' * tamanho_tabulacao)
         print(' ')
 
-    tentativa = str(input('Digite uma letra: ')).strip().lower()
-    validacao = validacaoEntrada(tentativa, letras_proibidas, letras_digitadas)
-    tentativa = validacao
+    letra_digitada = str(input('Digite uma letra: ')).strip().lower()
 
-    if tentativa == palavra:
+    while validacaoEntrada(letra_digitada, letras_digitadas) == False:
+        letra_digitada = str(input('Digite uma letra: ')).strip().lower()
+
+    if letra_digitada == palavra:
         break
-    elif tentativa not in letras_da_palavra:
+    elif letra_digitada not in letras_da_palavra:
         print('Você errou!')
         print()
         vida -= 1
 
-    while tentativa in letras_da_palavra:
-        palavra_misteriosa[letras_da_palavra.index(tentativa)] = tentativa
-        letras_da_palavra[letras_da_palavra.index(tentativa)] = '-'
+    while letra_digitada in letras_da_palavra:
+        palavra_misteriosa[letras_da_palavra.index(
+            letra_digitada)] = letra_digitada
+        letras_da_palavra[letras_da_palavra.index(letra_digitada)] = '-'
         acertos = ''.join(palavra_misteriosa)
 
-    letras_digitadas.append(tentativa)
+    letras_digitadas.append(letra_digitada)
 
-if tentativa == palavra:
+# Para que apresente a palavra no final
+if letra_digitada == palavra:
     palavra_misteriosa = palavra
 
+# Apresenta a palavra para o jogador
 for tracinho in palavra_misteriosa:
     print(tracinho, end=' ')
 print()
 
-if (acertos == palavra) or (tentativa == palavra):
+if (acertos == palavra) or (letra_digitada == palavra):
     print('Parabéns, você venceu!')
 else:
     print('Você perdeu :(')
---
+++ readme.md
@@ -0,0 +1,46 @@
+# Jogo da forca
+
+Jogo da forca desenvolvido em python. Tente acertar a palavra misteriosa.
+
+<br>
+
+```
+---------------------------------
+          JOGO DA FORCA
+---------------------------------
+Dica: Sobe, sobe, sobe e jamais desce.
+
+_ _ _ _ _
+Vida:  6
+Digite uma letra:
+```
+
+# Regras de jogo
+
+- Advinhe uma palavra aleatória.
+- Tenha um dica/ charada.
+- Tenha 6 vidas.
+- Cada letra faltando da palavra misteriosa será uma anderline.
+- Visualize as letras já digitadas.
+- Poderá digitar a palavra completa
+
+# Passos do desenvolvimento
+
+- [x] Base de dados em txt.
+  - Palavras e dicas separado por ponto e vírgula (;).
+- [x] Receber input do usuario e validar entrada.
+  - Aceitar apenas letras.
+  - Não aceitar letras/ palavras já digitadas.
+  - Não aceitar caracter acentuado.
+  - Não aceita a letra "ç"
+- [x] Implementar cada regra de jogo.
+- [x] Mostrar palavra no final.
+- [x] Criar loop para o jogo continuar até acerta a palavra ou até acabar a vida.
+
+# Ideias para atualização
+
+- [ ] Poder advinha quantas palavras quiser.
+  - No final do programa, mostrar percas, acertos e palavras acertadas e erradas.
+- [ ] Poder jogar com mais pessoas.
+  - Poder escolher entre os demais jogadores, se eles tentam advinhar a mesma palavra ou outra.
+  - 6 vidas para cada jogador.
